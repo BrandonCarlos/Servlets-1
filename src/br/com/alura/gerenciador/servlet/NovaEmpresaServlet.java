b56b580e142150a9f1950810e93cd77234a2240b
BrandonCarlos <brandon_marzo@hotmail.com>
2022-07-09 11:04:52 -0300

    Listando Empresas com Servlet

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -18,6 +18,12 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 		System.out.println("Cadastrando nova empresa");
 		String nomeDaEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeDaEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa  " + nomeDaEmpresa +  " cadastrada com sucesso!</body></html>");	
 	}
